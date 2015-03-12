98cb6a4dbe6cd601d5e7b1b691da9d1f67859dc0
Paul Love <paulqlove@gmail.com>
2015-03-12 14:11:05 -0500

    changed up title to paullove and added icon

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Laravel Blog</title>
+		<title>Paul Love - Developer</title>
 		<meta charset="utf-8">
 			
 		  <meta name="viewport" content="width=device-width, initial-scale=1">
 		<!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
 		<link href='http://fonts.googleapis.com/css?family=Roboto:100,300,400,500,500italic,700,100' rel='stylesheet' type='text/css'>
+		<link rel="icon" type="image/png" href="/images/logoIcon.png">
 		<link rel="stylesheet" type="text/css" href="/css/bootstrap/css/bootstrap.min.css">
 		<link rel="stylesheet" type="text/css" href="/css/custom.css">
 		
