38033cb9f1c10e73c44e9595190259558447c421
Paul Love <paulqlove@gmail.com>
2015-03-24 19:39:59 -0500

    fixed footer

--- app/views/partials/footer.blade.php
+++ app/views/partials/footer.blade.php
@@ -1,19 +1,17 @@
-<footer>
-	<div class="col-md-12">
+<footer class="col-md-12">
 		<div class="row">
-		<div class="col-md-12" id="footerspacing">
-			<ul>
-				<li class="icons" ><a href="https://linkedin.com/in/paulqlove" target="_blank">
-						<img  class="icons" src="/icons/social123.svg"></img>
-					</a></li>
-				<li class="icons"  ><a href="https://github.com/paulqlove" target="_blank">
-						<img  class="icons" src="/icons/github7.svg"></img>
-					</a></li>
-				<li class="icons" ><a href="mailto:paulqlove@gmail.com">
-						<img  class="icons" src="/icons/social113.svg"></img>
-					</a></li>
-			</ul>
+			<div class="col-md-12" id="footerspacing">
+				<ul>
+					<li class="icons" ><a href="https://linkedin.com/in/paulqlove" target="_blank">
+							<img  class="icons" src="/icons/social123.svg"></img>
+						</a></li>
+					<li class="icons"  ><a href="https://github.com/paulqlove" target="_blank">
+							<img  class="icons" src="/icons/github7.svg"></img>
+						</a></li>
+					<li class="icons" ><a href="mailto:paulqlove@gmail.com">
+							<img  class="icons" src="/icons/social113.svg"></img>
+						</a></li>
+				</ul>
+			</div>
 		</div>
-		</div>
-	</div>
 </footer>
