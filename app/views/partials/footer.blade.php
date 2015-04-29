d1ecbf6fe86b8c555b400a361bf89431b19b8518
Paul Love <paulqlove@gmail.com>
2015-04-29 09:58:06 -0500

    added spotify link

--- app/views/partials/footer.blade.php
+++ app/views/partials/footer.blade.php
@@ -3,13 +3,17 @@
 			<div class="col-md-12" id="footerspacing">
 				<ul>
 					<li class="icons" ><a href="https://linkedin.com/in/paulqlove" target="_blank">
-							<img  class="icons" src="/icons/social123.svg"></img>
+							<img  class="icons" src="/icons/social123.svg">
 						</a></li>
 					<li class="icons"  ><a href="https://github.com/paulqlove" target="_blank">
-							<img  class="icons" src="/icons/github7.svg"></img>
+							<img  class="icons" src="/icons/github7.svg">
 						</a></li>
 					<li class="icons" ><a href="mailto:paulqlove@gmail.com">
-							<img  class="icons" src="/icons/social113.svg"></img>
+							<img  class="icons" src="/icons/social113.svg">
+						</a></li>
+					<li class="icons">
+						<a href="https://open.spotify.com/user/paulqlove/playlist/1550ncVsi83u5I5Az7klyy" target="_blank">
+							<img src="/icons/spotify6.svg">
 						</a></li>
 				</ul>
 			</div>
