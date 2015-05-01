94fe01860d728caaf14c3d6fe6218c479152b5ee
Paul Love <paulqlove@gmail.com>
2015-05-01 14:54:22 -0500

    fixed footer

--- app/views/partials/footer.blade.php
+++ app/views/partials/footer.blade.php
@@ -1,17 +1,17 @@
 <footer class="col-md-12">
 		<div class="row">
-			<div class="col-md-12" id="footerspacing">
+			<div class="col-md-offset-2 col-md-10" id="footerspacing">
 				<ul>
-					<li class="icons" ><a href="https://linkedin.com/in/paulqlove" target="_blank">
+					<li class="col-xs-2 col-sm-2 icons" ><a href="https://linkedin.com/in/paulqlove" target="_blank">
 							<img  class="icons" src="/icons/social123.svg">
 						</a></li>
-					<li class="icons"  ><a href="https://github.com/paulqlove" target="_blank">
+					<li class="col-xs-2 col-sm-2 icons"  ><a href="https://github.com/paulqlove" target="_blank">
 							<img  class="icons" src="/icons/github7.svg">
 						</a></li>
-					<li class="icons" ><a href="mailto:paulqlove@gmail.com">
+					<li class="col-xs-2 col-sm-2 icons" ><a href="mailto:paulqlove@gmail.com">
 							<img  class="icons" src="/icons/social113.svg">
 						</a></li>
-					<li class="icons">
+					<li class="col-xs-2 col-sm-2 icons">
 						<a href="https://open.spotify.com/user/paulqlove/playlist/1550ncVsi83u5I5Az7klyy" target="_blank">
 							<img src="/icons/spotify6.svg">
 						</a></li>
